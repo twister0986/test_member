9136003e9603ee44603bab0291fcc72fa305daf4
twister0986 <twister0986@gmail.com>
2024-11-14 12:00:12 +0800

    Initial commit

---
+++ app_link_test.py
@@ -0,0 +1,37 @@
+import requests
+import zipfile
+import os
+import streamlit as st
+import sqlite3
+
+# 定義資料庫壓縮檔的 GitHub URL
+GITHUB_URL = "https://github.com/twister0986/test_member/raw/main/member_data_v3.zip"
+
+# 定義下載和解壓的路徑
+zip_path = "member_data_v3.zip"
+db_path = "member_data_v3.db"
+
+# 檢查是否已經存在解壓縮的資料庫檔案
+if not os.path.exists(db_path):
+    # 從 GitHub 下載壓縮檔
+    with requests.get(GITHUB_URL, stream=True) as r:
+        with open(zip_path, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+    # 解壓縮檔案
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall()
+
+    # 移除壓縮檔
+    os.remove(zip_path)
+
+conn = sqlite3.connect('member_data_v3.db')
+cursor = conn.cursor()
+
+cursor.execute('SELECT * FROM `UU-1001`;')
+
+st.write(len(cursor.fetchall()))
+tables=cursor.fetchall()
+
+conn.close()
